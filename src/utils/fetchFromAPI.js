1dc38a11f6c0dea829ea5cf4ded7a87262cf8d21
Daniyal <daniyalahmad1303@gmail.com>
2023-05-10 11:22:42 +0500

    Add existing project files to Git

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '93778451c9msh50cb7b8b29818a4p117d33jsndf74e3260661',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
